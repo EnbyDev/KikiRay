ad48fd0ad25a1ab14556fb3a7615e090877477e9
EnbyDev <96933866+EnbyDev@users.noreply.github.com>
2024-01-14 00:22:02 -0800

    testing pipeline
    testing pipeline

--- homepage.js
+++ homepage.js
@@ -1,21 +1,44 @@
 
 export default {
+  
   setup() {
-    return { }
+    return {}
   },
   template: `
   
-  <div class="container">
-  <h1>Welcome to KikiRay's multifaceted services!</h1>
-  <p> We are your go-to destination for a seamless blend of cleaning, photography, and organizational expertise.<br>
-  At the heart of our business is a commitment to transforming spaces into pristine havens, and capturing  beauty through our lenses. <br>
-  Whether you're seeking a spotless home, striking visual memories, or an organized environment, we bring a tailored approach to meet your unique needs.<br>
-  Elevate your surroundings with our comprehensive services, where cleanliness meets creativity and order meets aesthetics.<br>
-  Explore the synergy of cleanliness, photography, and organization with KikiRay.</p>
 
- 
-  <!-- <button type="button" class="btn btn-primary">Submit</button> ADD EMAIL CONTACT FORM -->
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">KikiRay</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#">Photography</a>
+    <a class="p-2 text-dark" href="#">Cleaning</a>
+    <a class="p-2 text-dark" href="#">Pricing</a>
+  </nav>
 </div>
+
+<body>
+
+<div class="container">
+<h1>Welcome to KikiRay's multifaceted services!</h1>
+<p> We are your go-to destination for a seamless blend of cleaning, photography, and organizational expertise.<br>
+At the heart of our business is a commitment to transforming spaces into pristine havens, and capturing  beauty through our lenses. <br>
+Whether you're seeking a spotless home, striking visual memories, or an organized environment, we bring a tailored approach to meet your unique needs.<br>
+Elevate your surroundings with our comprehensive services, where cleanliness meets creativity and order meets aesthetics.<br>
+Explore the synergy of cleanliness, photography, and organization with KikiRay.</p>
+
+
+<!-- <button type="button" class="btn btn-primary">Submit</button> ADD EMAIL CONTACT FORM -->
+</div>
+
+</body>
+
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        <small class="d-block mb-3 text-muted">&copy; 2024 Kikiray.com / Kimberly Ford</small>
+      </div>
+    </div>
+  </footer>
 
 <!-- Bootstrap JS (optional, only if you need Bootstrap JavaScript features) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
