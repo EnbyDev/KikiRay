7a8d52d750bd38ea00ef60d61ef798ed2aa7093e
EnbyDev <96933866+EnbyDev@users.noreply.github.com>
2024-01-13 23:18:06 -0800

    base templating and setup
    VUE 3 with Bootstrap 5 and ES / VanillaJS

---
+++ homepage.js
@@ -0,0 +1,25 @@
+
+export default {
+  setup() {
+    return { }
+  },
+  template: `
+  
+  <div class="container">
+  <h1>Welcome to KikiRay's multifaceted services!</h1>
+  <p> We are your go-to destination for a seamless blend of cleaning, photography, and organizational expertise.<br>
+  At the heart of our business is a commitment to transforming spaces into pristine havens, and capturing  beauty through our lenses. <br>
+  Whether you're seeking a spotless home, striking visual memories, or an organized environment, we bring a tailored approach to meet your unique needs.<br>
+  Elevate your surroundings with our comprehensive services, where cleanliness meets creativity and order meets aesthetics.<br>
+  Explore the synergy of cleanliness, photography, and organization with KikiRay.</p>
+
+ 
+  <!-- <button type="button" class="btn btn-primary">Submit</button> ADD EMAIL CONTACT FORM -->
+</div>
+
+<!-- Bootstrap JS (optional, only if you need Bootstrap JavaScript features) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+  
+  `
+}
